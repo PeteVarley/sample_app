ca97a2a9e988218c2897b1dafac7fb3d1f89ff85
Varley <petevarley@gmail.com>
2014-09-22 15:54:49 -0400

    resolving

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,19 +13,8 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-<<<<<<< HEAD
   test "should get about" do 
   	get :about
   	assert_response :success
   end
-
 end
-=======
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
-
-end
->>>>>>> static_pages
