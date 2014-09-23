e3628cfa823240c559fa508791f0c8e7122821ac
Varley <petevarley@gmail.com>
2014-09-22 23:30:22 -0400

    4.7 Green

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
-def setup
-	@base_title = "Ruby on Rails Tutorial Sample App"
-end
-	
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -18,9 +18,9 @@ end
     assert_select "title", "Help | #{@base_title}"
   end
 
-  test "should get about" do 
-  	get :about
-  	assert_response :success
-  	assert_select "title", "About | #{@base_title}"
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
